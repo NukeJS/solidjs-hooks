f7098c599f5c839b3e41c88f30b06df2669486c8
ronniew <rwaebers@gmail.com>
2022-02-03 22:59:37 +0100

    New hooks

--- packages/solidjs-hooks/src/index.ts
+++ packages/solidjs-hooks/src/index.ts
@@ -1,4 +1,6 @@
 export { default as useBoolean } from './useBoolean';
 export { default as useCounter } from './useCounter';
+export { default as useInterval } from './useInterval';
 export { default as useNumber } from './useNumber';
+export { default as useTimeout } from './useTimeout';
 export { default as useToggle } from './useToggle';
