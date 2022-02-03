302a34b831ee7fd937c4272def28629aaab23852
ronniew <rwaebers@gmail.com>
2022-02-03 23:32:46 +0100

    fix `useTimeout` hook delay

--- packages/solidjs-hooks/src/useTimeout.ts
+++ packages/solidjs-hooks/src/useTimeout.ts
@@ -1,9 +1,15 @@
 import type { Accessor } from 'solid-js';
 import { createEffect, onCleanup } from 'solid-js';
 
-function useTimeout(callback: () => void, delay: Accessor<number>) {
+function useTimeout(callback: () => void, delay?: Accessor<number> | null) {
   createEffect(() => {
-    if (typeof delay() !== 'number' || delay() < 0) return;
+    if (
+      delay === null ||
+      delay === undefined ||
+      typeof delay() !== 'number' ||
+      delay() < 0
+    )
+      return;
 
     const timeout = setTimeout(callback, delay());
 
