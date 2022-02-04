5243597c3b36537a20bbf55814e0dffe7589128b
ronniew <rwaebers@gmail.com>
2022-02-04 01:29:30 +0100

    Created utilities

---
+++ packages/solidjs-hooks/src/utils/index.ts
@@ -0,0 +1,7 @@
+export function isDefined<T>(value: T | undefined | null): value is T {
+  return typeof value !== 'undefined' && value !== null;
+}
+
+export function isFunction<T>(value: T | Function): value is Function {
+  return typeof value === 'function';
+}
